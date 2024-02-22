bab098ad46b281bd9e4892c4e7315ba6a1a1066d
GuQli <gklibadze@gmail.com>
2024-02-22 12:31:34 +0400

    netlifytoml

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,10 +4,7 @@ export const HomePage = () => {
   return (
     <main className="dark:bg-gray-800">
       <Hero />
-
       <Projects />
     </main>
   );
 };
-
-// <div className="text-black w-100 dark:text-white">      </div>

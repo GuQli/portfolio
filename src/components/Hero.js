7deec895aecd4c2dfe627ae0c42f7377f3db9520
GuQli <gklibadze@gmail.com>
2024-03-15 12:02:02 +0400

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,8 @@ export const Hero = () => {
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 items-center md:gap-20">
         <div className="content">
           <p className="text-[40px] lg:text-[45px] xl:text-[55px] font-bold leading-tight mt-5 sm:mt-0 ">
-            I'm Guga Qlibadze <br />
+            I'm Guga Qlibadze AEEEEEEEEEEEE
+            <br />
             <span>Web Developer</span>
           </p>
           <p className="mt-5 md:text-md ">
